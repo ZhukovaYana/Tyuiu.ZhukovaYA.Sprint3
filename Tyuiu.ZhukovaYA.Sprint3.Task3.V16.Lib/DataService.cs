83967f9297aec007e4d2643f2aced0784a5f4233
Yana <yana_zhukova2004@mail.ru>
2024-11-01 17:27:03 +0500

    Task3

---
+++ Tyuiu.ZhukovaYA.Sprint3.Task3.V16.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ZhukovaYA.Sprint3.Task3.V16.Lib
+{
+    public class DataService : ISprint3Task3V16
+    {
+        public int GetCharCount(string value, char item)
+        {
+            int count = 0;
+            foreach (char c in value) 
+            {
+            if (c == item)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.ZhukovaYA.Sprint3.Task3.V16/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.ZhukovaYA.Sprint3.Task3.V16.Lib;
+namespace Tyuiu.ZhukovaYA.Sprint3.Task3.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Жукова Яна Андреевна | ИИПБ-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Оператор цикла foreach                                                  *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Жукова Яна Андреевна | ИИПБ-24-1                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Используя цикл foreach подсчитать количество букв “c” в строке ice nice  *");
+            Console.WriteLine("*ice creamcc.                                                             *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите текст:");
+            string x = Convert.ToString(Console.ReadLine()!);
+            Console.WriteLine("Введите букву:");
+            char y = Convert.ToChar(Console.ReadLine()!);
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine(ds.GetCharCount(x, y));
+            Console.ReadKey();
+        }
+    }
+}
